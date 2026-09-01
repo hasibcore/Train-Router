2249eac6f84c43c54f4467268d981fd65b30da9a
arhum-diganta <arhum.rahman.2006@gmail.com>
2026-09-01 23:37:14 +0600

    Further tidying up the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:train_router/under_construction_page.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -17,65 +17,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class TrainRouterHome extends StatelessWidget {
-  const TrainRouterHome({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 1, 162, 255),
-        elevation: 0,
-        centerTitle: true,
-        leading: IconButton(
-          onPressed: () {},
-          icon: const Icon(
-            Icons.menu,
-            color: Colors.white,
-          ),
-        ),
-
-        title: const Text(
-          'TRAIN ROUTER',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 0.8,
-          ),
-        ),
-      ),
-      body: Center(
-        child: Column(mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                Icons.train,
-                size: 150,
-                color: Color.fromARGB(255, 1, 162, 255),
-              ),
-              SizedBox(height: 15),
-              Text(
-                'Page is Under Development',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              
-              
-              TextButton(
-                style: ButtonStyle(
-                  foregroundColor: WidgetStatePropertyAll(const Color.fromARGB(255, 26, 3, 238)),
-                ),
-                onPressed: () {},
-                child: Text('Click Here To go Home'),
-              )
-
-            ]
-        ),
-        
-      ),
-      
-    );
-  }
-}
 /*
 Login page
 Sign up page
