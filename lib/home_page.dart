11da6e5155b5aa619b4872df6ad8d7b76f065e13
arhum-diganta <arhum.rahman.2006@gmail.com>
2026-09-04 21:51:07 +0600

    Planning of the home page

---
+++ lib/home_page.dart
@@ -0,0 +1,10 @@
+/*
+Make a bg image for the body
+put the basic features on the page
+in the sidebar menu, put all of the features (somehow make it across all screens)
+start with the basic info of the app
+then buttons for:
+1. Fare calculator
+2. Seat Classes
+3. Seat images
+ */
