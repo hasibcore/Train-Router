21e02040a024940dab5e17fb6ff5f599171f4e02
arhum-diganta <arhum.rahman.2006@gmail.com>
2026-09-19 00:50:50 +0600

    Updated the home page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:train_router/under_construction_page.dart';
+import 'custom_widgets/app_bar.dart';
+import 'custom_widgets/app_drawer.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const CustomAppBar(title: 'Home'),
+      drawer: const AppDrawer(),
+
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 15),
+            const CircleAvatar(
+              radius: 35,
+              backgroundColor: Colors.grey,
+              child: Icon(Icons.person, size: 50, color: Colors.white),
+            ),
+            const SizedBox(height: 12),
+            Row(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'Cristiano Ronaldo',
+                      style: TextStyle(
+                        fontSize: 26,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 4),
+                    Text(
+                      '01777777777',
+                      style: TextStyle(fontSize: 22, color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+
+            const SizedBox(height: 24),
+
+            // Trips section (acts as a button)
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => HomePage()),
+                    );
+                  },
+                  child: const Text(
+                    'Your Trips:',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.symmetric(vertical: 80),
+                  decoration: BoxDecoration(
+                    color: Colors.blue[50],
+                    borderRadius: BorderRadius.circular(12),
+                    border: Border.all(color: Colors.blue[100]!),
+                  ),
+
+                  child: const Center(
+                    child: Text(
+                      'You have made no trips!',
+                      style: TextStyle(fontSize: 20, color: Colors.black54),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 24),
+                Center(
+                  child: SizedBox(
+                    width: 250,
+                    height: 60,
+
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => UnderConstruction(),
+                          ),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.greenAccent,
+                        foregroundColor: Colors.white,
+                      ),
+                      child: const Text(
+                        'Fare Calculator',
+                        style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 /*
 Make a bg image for the body
 put the basic features on the page
@@ -8,17 +135,3 @@ then buttons for:
 2. Seat Classes
 3. Seat images
  */
-import 'package:flutter/material.dart';
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-class _HomePageState extends State<HomePage>{
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      
-    );
-  }
-}
