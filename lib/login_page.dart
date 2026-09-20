1d3d735b7b23bbc9d084e0333e2736d4f865cc05
arhum-diganta <arhum.rahman.2006@gmail.com>
2026-09-21 00:13:54 +0600

    Updated the login screen

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,38 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:train_router/home_page.dart';
+
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
-class _LoginPageState extends State<LoginPage>{
+
+class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlue,
       body: Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
               'Welcome back! Sign in to continue',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
             ),
-            TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Username',
+            SizedBox(height: 15),
+            SizedBox(
+              height: 50,
+              width: 400,
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Username',
+                  filled: true,
+                  fillColor: Colors.white,
+                ),
+              ),
             ),
-            
-          ),
-          SizedBox(height: 10,),
-          TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'Password',
+            SizedBox(height: 10),
+            SizedBox(
+              height: 50,
+              width: 400,
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Password',
+                  filled: true,
+                  fillColor: Colors.white,
+                ),
+              ),
             ),
-            
-          ),
-          ]
-          )
+            SizedBox(height: 10,),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomePage()),
+                );
+              },
+              child: Text('Login'),
+            ),
+          ],
+        ),
       ),
     );
   }
