9f23aec4e823fc37232f402fcecbdf825578e9a1
arhum-diganta <arhum.rahman.2006@gmail.com>
2026-09-25 22:59:22 +0600

    Progress on the sign up page

--- lib/sign_up_page.dart
+++ lib/sign_up_page.dart
@@ -1,3 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:train_router/home_page.dart';
 import 'package:train_router/login_page.dart';
+
+class signUpPage extends StatefulWidget {
+  const signUpPage({super.key});
+  @override
+  State<signUpPage> createState() => _signUpPage();
+}
+class _signUpPage extends State<signUpPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlue,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+          ],
+        ),
+      ),
+    );
+  }
+}
